63b116e4732ab414eb372f8a07c13934211ece63
Ahmed Gamal <ahmed.gamal@starloopstudios.com>
2021-10-04 21:27:23 +0200

    Add simple recording Animation

--- Assets/Scripts/VideoProduction/VideoProductionViewController.cs
+++ Assets/Scripts/VideoProduction/VideoProductionViewController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class VideoProductionViewController : MonoBehaviour
@@ -12,7 +13,8 @@ public class VideoProductionViewController : MonoBehaviour
 
     [SerializeField] private TMP_Text selectedTheme;
     [SerializeField] private GameObject preProductionPanel, productionPanel, postProduction;
-      
+    [SerializeField] private Button recordButton;
+    [SerializeField] private Image recording_image;
     public void OnSelectTheme(string themeName)
     {
         _signalBus.TryFire<SelectThemeSignal>(new SelectThemeSignal()
@@ -22,18 +24,35 @@ public class VideoProductionViewController : MonoBehaviour
             
         });
         selectedTheme.text = "Theme : " + themeName;
+        recordButton.interactable = true;
     }
 
     public void OnStartRecordingPressed()
     {
+        preProductionPanel.SetActive(false);
+        productionPanel.SetActive(true);
+        StartCoroutine(FillTheRecordImage(10));
+    }
+
+    IEnumerator FillTheRecordImage(float time)
+    {
+
+        float tACC = 0;
+        while (tACC<time)
+        {
+            yield return new WaitForEndOfFrame();
+            tACC += Time.deltaTime;
+            recording_image.fillAmount=tACC/time;
+        }
         
     }
 
    
     void Start()
     {
-        
+        recordButton.onClick.AddListener(OnStartRecordingPressed);
     }
+    
 
     void Update()
     {
