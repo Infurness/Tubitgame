f5350323b6f0dc4be228836052fb2921984d98dc
Jorge Cásedas <jorge.casedas@startloopstudios.com>
2021-10-06 17:00:38 +0200

    PostproductionPanel fixes
    UI Fixes

--- Assets/Scripts/VideoProduction/YouTubeVideoManager.cs
+++ Assets/Scripts/VideoProduction/YouTubeVideoManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YouTubeVideoManger : MonoBehaviour
+public class YouTubeVideoManager : MonoBehaviour
 {
     
     void Start()
