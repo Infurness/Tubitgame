f7115b0a36631b0fdfa20002c6c4db1dd7418299
Jorge Cásedas <jorge.casedas@startloopstudios.com>
2021-10-07 11:01:41 +0200

    PostProduction ViewController

--- Assets/Scripts/VideoProduction/VideoPostProductionViewController.cs
+++ Assets/Scripts/VideoProduction/VideoPostProductionViewController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Zenject;
 
 public class VideoPostProductionViewController : MonoBehaviour
 {
     [Inject] private SignalBus _SignalBus;
     [SerializeField] private GameObject postProductionPanel;
-    void Start()
+
+    [SerializeField] private TMP_Text videoName;
+    [SerializeField] private TMP_Text videoViews;
+    [SerializeField] private TMP_Text videoLikes;
+    [SerializeField] private TMP_Text videoComments;
+    [SerializeField] private TMP_Text videoNewSubscribers;
+
+    void Start ()
     {
         _SignalBus.Subscribe<StartPublishSignal> (PublishVideo);
     }
@@ -21,5 +29,22 @@ public class VideoPostProductionViewController : MonoBehaviour
     {
         postProductionPanel.SetActive (true);
         Debug.Log ("Video published");
+
+        Video dummyVideo = new Video ();
+        dummyVideo.name = "Dummy video";
+        dummyVideo.views = 1234;
+        dummyVideo.likes = 82;
+        dummyVideo.comments = 30;
+        dummyVideo.newSubscribers = 4;
+        ShowVideoStats (dummyVideo);
+    }
+
+    void ShowVideoStats (Video _video)
+    {
+        videoName.text = _video.name;
+        videoViews.text = $"Views: {_video.views}";
+        videoLikes.text = $"Likes: {_video.likes}";
+        videoComments.text = $"Comments: {_video.comments}";
+        videoNewSubscribers.text = $"Subscribers: {_video.newSubscribers}";
     }
 }
