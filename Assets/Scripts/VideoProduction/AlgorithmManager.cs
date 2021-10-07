0c8daa3dfaf19b5eb0da7caf295ee7a55ad5aa6e
Jorge Cásedas <jorge.casedas@startloopstudios.com>
2021-10-07 11:10:29 +0200

    Video stats algorithms

--- Assets/Scripts/VideoProduction/AlgorithmManager.cs
+++ Assets/Scripts/VideoProduction/AlgorithmManager.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 public class AlgorithmManager : MonoBehaviour
 { 
     
-    
-    void Start()
+    public int GetVideoViews (int _base, int _subscribers, float[] _themes, float _videoQuality)
     {
-        
+        float themesPopularity = 0;
+        foreach(float theme in _themes)
+        {
+            themesPopularity += theme;
+        }
+
+        int viewers =(int)((_base + _subscribers) + ((_base + _subscribers) * themesPopularity * _videoQuality));
+        return viewers;
     }
 
-    void Update()
+    public int GetVideoLikes (int _views, int _videoQuality)
     {
-        
+        return (int)(_views * (_videoQuality*0.05f));
+    }
+    public int GetVideoComments (int _views)
+    {
+        return (int)(_views * 0.04f);
+    }
+    public int GetVideoSubscribers (int _views, int _videoQuality)
+    {
+        return (int)(_views *((_videoQuality*0.2f)*0.01f));
     }
 }
