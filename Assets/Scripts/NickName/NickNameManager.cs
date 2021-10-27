540380329a979f9a354579dec2278bb9e41d008b
Jorge Cásedas <jorge.casedas@startloopstudios.com>
2021-10-27 12:29:05 +0200

    New scene for nickname input

---
+++ Assets/Scripts/NickName/NickNameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class NickNameManager : MonoBehaviour
+{
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private TMP_Text nickNameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmButton.onClick.AddListener (OnConfirm);
+    }
+    void OnConfirm ()
+    {
+        UpdatePlayerName ();
+        StartCoroutine (LoadSceneAsync (2));
+    }
+    public void UpdatePlayerName ()
+    {
+        PlayFabClientAPI.UpdateUserTitleDisplayName (new UpdateUserTitleDisplayNameRequest
+        {
+            DisplayName = nickNameText.text
+        }, result =>
+        {
+            Debug.Log ("The player's display name is now: " + result.DisplayName);
+        }, error => Debug.LogError (error.GenerateErrorReport ()));
+    }
+    IEnumerator LoadSceneAsync (int index)
+    {
+        yield return new WaitForSecondsRealtime (3);
+        yield return UnityEngine.SceneManagement.SceneManager.LoadSceneAsync (index);
+
+    }
+
+}
+
