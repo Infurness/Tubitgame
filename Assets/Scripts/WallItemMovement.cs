30e4f0831f4f96f03dbfe49022e1c4d81b80c90f
Ahmed Gamal <ahmed.gamal@starloopstudios.com>
2021-12-23 01:22:27 +0200

    Snap to Floor and Wall By colliders

--- Assets/Scripts/WallItemMovement.cs
+++ Assets/Scripts/WallItemMovement.cs
@@ -6,13 +6,15 @@ using UnityEngine.EventSystems;
 using Zenject;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class FloorItemMovement : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+public class WallItemMovement : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
 {
     [Inject] private SignalBus signalBus;
+    
     private bool pointerDown;
     [SerializeField] private float speed = 0.5f;
     private Vector3 mospos;
     [SerializeField] private bool editMode = false;
+    [SerializeField] private LayerMask mask;
     private Rigidbody2D rigidbody2D;
     [SerializeField] private Collider2D collider;
    private void Awake()
@@ -26,12 +28,13 @@ public class FloorItemMovement : MonoBehaviour,IPointerDownHandler,IPointerUpHan
 
 
 
-   
    void Start()
     {
-
+        //    maxPos = transform.TransformVector(maxPos);
+       //  minPos = transform.TransformVector(minPos);
        mospos = Input.mousePosition;
- 
+   
+
        rigidbody2D = GetComponent<Rigidbody2D>();
        collider.isTrigger = true;
     }
@@ -46,8 +49,6 @@ public class FloorItemMovement : MonoBehaviour,IPointerDownHandler,IPointerUpHan
             if (pointerDown)
             {
                 var delta = Input.mousePosition - mospos;
-
-
                 rigidbody2D.AddForce(delta.normalized * speed, ForceMode2D.Force);
             }
         }
@@ -76,11 +77,22 @@ public void OnPointerDown(PointerEventData eventData)
         collider.isTrigger = true;
     }
 
-
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         rigidbody2D.velocity=Vector2.zero;
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer==LayerMask.NameToLayer("RightWall"))
+        {
+            var scale =Mathf.Abs(transform.localScale.x);
+            transform.localScale = new Vector3(-scale, scale, scale);
+        }else if (other.gameObject.layer==LayerMask.NameToLayer("LeftWall"))
+        {
+            var scale =Mathf.Abs(transform.localScale.x);
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
     }
 }
