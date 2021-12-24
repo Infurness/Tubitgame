491dd5c81605ea134d8eaa503e7e389b7a51fe9b
JorgeCasedasStarloop <jorge.casedas@starloopstudios.com>
2021-12-24 12:11:28 +0100

    Button VFX

--- Assets/Scripts/VFX/ButtonAnimationTrigger.cs
+++ Assets/Scripts/VFX/ButtonAnimationTrigger.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEditor;
 
+[RequireComponent(typeof(Animator))]
 public class ButtonAnimationTrigger : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     Animator anim;
@@ -17,7 +20,8 @@ public class ButtonAnimationTrigger : MonoBehaviour, IPointerDownHandler, IPoint
     {
         if(anim)
         {
-            anim.Play ("Button_Pressed");
+            anim.SetTrigger("Pressed");
+            anim.ResetTrigger("Release");
         }
         else
         {
@@ -29,11 +33,36 @@ public class ButtonAnimationTrigger : MonoBehaviour, IPointerDownHandler, IPoint
     {
         if (anim)
         {
-            anim.Play ("Button_Release");
+            anim.SetTrigger("Release");
+            anim.ResetTrigger("Pressed");
         }
         else
         {
             Debug.LogError ($"Animator not found on{name}, cannot start VFX");
+        }
+    }
+
+    void Reset()
+    {
+        Debug.Log("A");
+        if(anim = GetComponent<Animator>())
+        {
+            RuntimeAnimatorController controller = null;
+            foreach (RuntimeAnimatorController asset in Resources.FindObjectsOfTypeAll(typeof(RuntimeAnimatorController)) as RuntimeAnimatorController[])
+            {
+                if (asset.name == "Buttons")
+                {
+                    controller = asset;
+                    break;
+                }    
+            }
+            Debug.Log(controller);
+            anim.runtimeAnimatorController = controller;
+            Debug.Log("Yes anim");
+        }
+        else
+        {
+            Debug.Log("No anim");
         }
     }
 }
