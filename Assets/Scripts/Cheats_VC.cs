5b22bde594b1d5fc5ec41b924f46be0e4cbe34cb
Jorge Cásedas <jorge.casedas@starloopstudios.com>
2022-01-04 19:03:28 +0100

    Cheats hide funcitonality

--- Assets/Scripts/Cheats_VC.cs
+++ Assets/Scripts/Cheats_VC.cs
@@ -8,6 +8,11 @@ public class Cheats_VC : MonoBehaviour
 {
     [Inject] CheatsManager cheatsManager;
 
+    [SerializeField] private GameObject cheatButtonsPanel;
+    [SerializeField] private Button hideCheatButtons;
+    float hideMeter;
+    int touchCount;
+
     [SerializeField] private Button add100EnergyButton;
     [SerializeField] private Button levelUpButton;
     [SerializeField] private Button resetXp;
@@ -20,6 +25,7 @@ public class Cheats_VC : MonoBehaviour
     // Start is called before the first frame update
     void Start ()
     {
+        hideCheatButtons.onClick.AddListener(()=>EnableButtonsVisibility(false));
         add100EnergyButton.onClick.AddListener (cheatsManager.Add100Energy);
         levelUpButton.onClick.AddListener (cheatsManager.Add1Level);
         resetXp.onClick.AddListener (cheatsManager.ResetExperience);
@@ -27,5 +33,23 @@ public class Cheats_VC : MonoBehaviour
         softCurrencyRewardedAd.onClick.AddListener (cheatsManager.SoftCurrencyRewardAd);
         softCurrencyCheatButton.onClick.AddListener(cheatsManager.Add100SoftCurrency);
         hardCurrencyCheatButton.onClick.AddListener(cheatsManager.Add100HardCurrency);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hideMeter = 0;
+            touchCount += 1;
+        }
+        hideMeter += Time.deltaTime;
+        if (hideMeter >0.2f)
+            touchCount = 0;
+        if (touchCount >= 3)
+            EnableButtonsVisibility(true);
+    }
+    void EnableButtonsVisibility(bool enable)
+    {
+        cheatButtonsPanel.SetActive(enable);
+        touchCount = 0;
     }
 }
