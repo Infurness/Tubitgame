7064f85ff8886f0283e4dffd802e3d75ca5331d8
Jorge Cásedas <jorge.casedas@starloopstudios.com>
2022-01-05 13:57:00 +0100

    Every button interact sound

--- Assets/Scripts/VFX/ButtonSoundTrigger.cs
+++ Assets/Scripts/VFX/ButtonSoundTrigger.cs
@@ -11,18 +11,36 @@ public class ButtonSoundTrigger : MonoBehaviour, IPointerDownHandler, IPointerUp
     [Inject] private GlobalAudioManager audioManager;
     [Inject] private SoundsHolder soundsHolder;
 
+    bool selectedAsUnique;
+    private void Awake()
+    {
+        ButtonSoundTrigger[] triggers = GetComponents<ButtonSoundTrigger>();
+        if (triggers.Length > 1) //In case a button has this script twice
+        {
+            bool alreadySelected = false;
+            foreach(ButtonSoundTrigger trigger in triggers)
+            {
+                if (trigger.selectedAsUnique)
+                    alreadySelected = true;
+            }
+            if (alreadySelected && !selectedAsUnique)
+                Destroy(this);
+            else if (!alreadySelected)
+                selectedAsUnique = true;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
     public void OnPointerDown (PointerEventData eventData)
     {
-
+        audioManager.PlaySound(soundsHolder.pushButton, AudioType.Effect);
     }
 
     public void OnPointerUp (PointerEventData eventData)
     {
-        audioManager.PlaySound(soundsHolder.pushButton, AudioType.Effect);
+        //audioManager.PlaySound(soundsHolder.pushButton, AudioType.Effect);
     }
 }
