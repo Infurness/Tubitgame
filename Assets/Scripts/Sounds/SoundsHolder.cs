7a092c431e32fd3e0ca2a78e4dafd50c59464cd4
Jorge Cásedas <jorge.casedas@starloopstudios.com>
2022-01-05 18:04:49 +0100

    Left sounds and audio sliders realtime binding

--- Assets/Scripts/Sounds/SoundsHolder.cs
+++ Assets/Scripts/Sounds/SoundsHolder.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class SoundsHolder : MonoBehaviour
 {
+    [Header("Music")]
+    public AudioClip shopMusic;
+    public AudioClip generalMusic;
+
+    [Header("Effects")]
     public AudioClip pushButton;
     public AudioClip popUp;
     public AudioClip rankUp;
     public AudioClip enterShop;
     public AudioClip buyItem;
+    public AudioClip streetSounds;
 }
