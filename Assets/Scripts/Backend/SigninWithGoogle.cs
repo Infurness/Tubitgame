816ec37f93d81dde6d2ab92324f31738a0755bfc
mblasquiz <miguel.blasquiz@gmail.com>
2022-06-03 11:29:39 +0200

    Test

--- Assets/Scripts/Backend/SigninWithGoogle.cs
+++ Assets/Scripts/Backend/SigninWithGoogle.cs
@@ -1,20 +1,60 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Google;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Networking;
 using Zenject;
+
+[Serializable]
+public class TokenClassName
+{
+    public string access_token;
+}
+
 
 public class SigninWithGoogle
 {
     private GoogleSignInConfiguration _configuration;
 
-    public SigninWithGoogle(string clintId = "786436489167-rtuno9jd4smvkstqqmv7kjsj4rvkfdtq.apps.googleusercontent.com")
+    private static void GetAccessToken(Action<string> result)
     {
+        var content = new Dictionary<string, string>();
+
+        //content.Add("grant_type", "client_credentials");
+        content.Add("client_id", "786436489167-rtuno9jd4smvkstqqmv7kjsj4rvkfdtq.apps.googleusercontent.com");
+        content.Add("client_secret", "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");
+        content.Add("redirect_uri", "https://oauth.playfab.com/oauth2/google");
+
+        var www = UnityWebRequest.Post("https://someurl.com//oauth/token", content);
+
+        www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.ConnectionError)
+        {
+            string resultContent = www.downloadHandler.text;
+            TokenClassName json = JsonUtility.FromJson<TokenClassName>(resultContent);
+
+            result(json.access_token);
+        }
+        else
+        {
+            result("");
+        }
+    }
+
+
+    public SigninWithGoogle(string clientID = "786436489167-rtuno9jd4smvkstqqmv7kjsj4rvkfdtq.apps.googleusercontent.com")
+    {
+#if UNITY_IOS
+clientID = "786436489167-vi6acu8rehq7ug9ghj2k22oa43q2sb7b.apps.googleusercontent.com";
+#endif
         _configuration = new GoogleSignInConfiguration()
         {
             RequestEmail = true,
-            WebClientId = clintId,
+            WebClientId = clientID,
             RequestAuthCode = true,
             RequestIdToken = true,
             ForceTokenRefresh = true, 
@@ -46,13 +86,24 @@ public class SigninWithGoogle
             await task;
             if (!task.IsFaulted)
             {
-                Debug.Log("Login with Google Success");
-                PlayerPrefs.SetString("GoogleUser", "googleUser");
-                PlayerPrefs.Save();
-                signalBus.Fire(new OnGoogleSignInSuccessSignal()
+                GetAccessToken((token)=>
                 {
-                    AuthCode = task.Result.AuthCode,
+                    if(!string.IsNullOrEmpty(token))
+                    {
+                        signalBus.Fire(new OnGoogleSignInSuccessSignal()
+                        {
+                            AuthCode = token,
+                        });
+                    }
                 });
+
+                // Debug.Log("Login with Google Success");
+                // PlayerPrefs.SetString("GoogleUser", "googleUser");
+                // PlayerPrefs.Save();
+                // signalBus.Fire(new OnGoogleSignInSuccessSignal()
+                // {
+                //     AuthCode = task.Result.AuthCode,
+                // });
             }
             else
             {
